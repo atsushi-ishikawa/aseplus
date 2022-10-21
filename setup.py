defbea94a89bdce61393d818c95a9a68bc4a7659
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2022-10-21 11:55:43 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aseplus",
+    version="0.0.0",
+    description="aseplus",
+
+    author="ishikawa",
+    author_email="ishikawa@gmail.com",
+    url="None.com",
+
+    packages=[
+        "aseplus",
+    ],
+)
