177b90e43e68670d7e1c164b49e6b95cb7c6d134
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2022-10-21 15:13:17 +0900

    first commit

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="aseplus",
     version="0.0.0",
     description="aseplus",
-
+    install_requires=[
+        "ase",
+        "numpy",
+        "pandas",
+    ],
     author="ishikawa",
     author_email="ishikawa@gmail.com",
     url="None.com",
